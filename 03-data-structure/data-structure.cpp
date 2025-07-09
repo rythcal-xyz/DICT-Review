63c067043ed9a32dd28b98be57974563dcbe8fb2
rythcal-xyz <royethcalcaben17@gmail.com>
2025-07-09 08:45:09 +0800

    Adding new info for Data Structure

---
+++ 03-data-structure/data-structure.cpp
@@ -0,0 +1,58 @@
+// what is Data? 
+
+// Data are just a collection of facts, quantities, characters, symbols and figures, 
+// or can say data are values or a set of values that are in a particular format 
+// which maay be stored and transmitted by a computer.
+
+// In computer terms, a data structure is a specific way to store and orgranize data 
+// in a computer's memory so that these data can be used efficiently later. 
+// Data may be arranged in many differnet ways/, such as the logical or mathematical model 
+// for a particular organization of data is termed as a data structure.
+
+// Example of it is an Array.
+
+
+// memberID  |  fullName                 |   gender    |      dob         |
+//-----------|---------------------------|-------------|------------------|  
+// 31321     |  Jeferson G. Mabulay      |   Male      |      3/12/2000   |
+// 31322     |  Shein Rose V. Montilla   |  Female     |      2/15/2004   |
+// 31323     |  Limuel A. Jabon          |   Male      |      4/23/2002   |        
+//-----------|---------------------------|-------------|------------------|
+
+
+// The variety of a specific data model depends on the two factors:
+
+// First
+// it must be loaded enough in structure to reflect the a
+// ctual relationships of the data with the real-world objects.
+
+// Example: 
+// Data of the student, eg. the enrolled student, 
+// it suppose to save the data to database.
+// and retrieving it using the student id.
+
+
+// Second 
+// the formation should be simple enough so that anyone can effieciently 
+// process the data each time it is necessary
+// It should be a friendly-use or it can be easily manipulate to the db.
+
+// Example: 
+// The student was graduated.
+// incorrect inputs and can be delete or update the data.
+
+
+// Categories of Data Structure
+
+// The data structure can be subdivided into major types:
+
+// Linear Data Structure 
+
+// Data Structure where data elements are arranged sequentially or linearly where the elements are attached to its previous and next adjacent in what is called linear data structure.
+// In linear data structure, single level is involved. Therefore, we can tranverse all the elements in single run 
+
+
+// Non-Linear Data Structure
+
+
+
