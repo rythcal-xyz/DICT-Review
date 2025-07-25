11845262c900f70bfa41ef861ee6315cc4df5488
rythcal-xyz <royethcalcaben17@gmail.com>
2025-07-25 16:22:13 +0800

    Guess the output 2

---
+++ 01-program-simulation/guessing-the-output/02-guess-the-output.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class GuessTheOuput2
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Liftoff!");
+    }
+}
