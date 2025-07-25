783791ebcec06be6a4f1bb579a7b5f1899a58c47
rythcal-xyz <royethcalcaben17@gmail.com>
2025-07-25 16:24:29 +0800

    Guess the output 5

---
+++ 01-program-simulation/guessing-the-output/05-guess-the-output.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class GuessTheOuput5
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                Console.Write($" {j} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+    
